003653ee62ea63a0540a2e4b1d27ea8efab585c8
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2025-06-17 17:39:13 +0530

    Checking Dynamic Metadata generation through Layout

--- app/blog/[slug]/layout.js
+++ app/blog/[slug]/layout.js
@@ -1,3 +1,30 @@
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const { slug } = await params;
+
+  const blog = await fetch(
+    `${process.env.NEXT_PUBLIC_BLOG_API_URL}/get/blog/${slug}`
+  ).then((res) => res.json());
+
+  return {
+    title: blog.blog.title,
+    description: blog.blog.excerpt,
+    metadataBase: new URL("https://deforge.io/blog/" + slug),
+    openGraph: {
+      title: blog.blog.title,
+      description: blog.blog.excerpt,
+      images: [
+        {
+          url: blog.blog.image,
+          width: 1200,
+          height: 800,
+          alt: blog.blog.title,
+        },
+      ],
+    },
+  };
+}
+
 export default function BlogLayout({ children }) {
   return <>{children}</>;
 }
--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,31 +1,5 @@
 import BlogBody from "@/components/blog/BlogBody";
 
-export async function generateMetadata({ params, searchParams }, parent) {
-  // read route params
-  const { slug } = await params;
-
-  const blog = await fetch(
-    `${process.env.NEXT_PUBLIC_BLOG_API_URL}/get/blog/${slug}`
-  ).then((res) => res.json());
-
-  return {
-    title: blog.blog.title,
-    description: blog.blog.excerpt,
-    openGraph: {
-      title: blog.blog.title,
-      description: blog.blog.excerpt,
-      images: [
-        {
-          url: blog.blog.image,
-          width: 1200,
-          height: 800,
-          alt: blog.blog.title,
-        },
-      ],
-    },
-  };
-}
-
 export default function BlogPostPage() {
   return <BlogBody />;
 }
