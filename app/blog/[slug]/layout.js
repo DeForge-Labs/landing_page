4a80437470191aae2390d5666ec131fb39cc8e25
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2025-06-17 17:47:32 +0530

    Testing out dynamic metadata

--- app/blog/[slug]/layout.js
+++ app/blog/[slug]/layout.js
@@ -6,6 +6,24 @@ export async function generateMetadata({ params, searchParams }, parent) {
     `${process.env.NEXT_PUBLIC_BLOG_API_URL}/get/blog/${slug}`
   ).then((res) => res.json());
 
+  console.log({
+    title: blog.blog.title,
+    description: blog.blog.excerpt,
+    metadataBase: new URL("https://deforge.io/blog/" + slug),
+    openGraph: {
+      title: blog.blog.title,
+      description: blog.blog.excerpt,
+      images: [
+        {
+          url: blog.blog.image,
+          width: 1200,
+          height: 800,
+          alt: blog.blog.title,
+        },
+      ],
+    },
+  });
+
   return {
     title: blog.blog.title,
     description: blog.blog.excerpt,
