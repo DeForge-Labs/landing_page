cec8cded9964d3cfb55b31e5f69f5f3bb276d43c
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2026-03-07 01:34:43 +0530

    feat: Update Product Hunt badge URL and image source to reflect new featured badge.

--- components/home/header/ProductHuntBadge.js
+++ components/home/header/ProductHuntBadge.js
@@ -4,13 +4,13 @@ const ProductHuntBadge = () => (
   <Link
     target="_blank"
     rel="noopener noreferrer"
-    href="https://www.producthunt.com/products/deforge-open-beta?embed=true&utm_source=badge-top-post-badge&utm_medium=badge&utm_source=badge-deforge"
+    href="https://www.producthunt.com/products/deforge-open-beta?embed=true&amp;utm_source=badge-featured&amp;utm_medium=badge&amp;utm_campaign=badge-deforge-2"
   >
     <img
       width={220}
       height={48}
       alt="Deforge - Canva&#0032;for&#0032;AI&#0032;Agents | Product Hunt"
-      src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=1007944&theme=dark&period=daily&t=1756041023938"
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1087631&amp;theme=light&amp;t=1772827367160"
       className="select-none"
     />
   </Link>
